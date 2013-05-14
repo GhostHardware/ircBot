fa2b5bc0b11337ae9189d873ea87e5e41d1ea540
GhostHardware <imaginary@email.com>
2013-05-14 23:29:21 +0200

    changed to what input bot responses

--- firstBot.py
+++ firstBot.py
@@ -39,27 +39,27 @@ while alive:
     if irc_msg.find(":Hello "+nick) != -1:
         irc.send(chan_msg+"Hello!\n")
 
-    if ":!git" in irc_msg:
+    if irc_msg == ":!git":
         irc.send(chan_msg+"https://github.com/fnurk/studypython \n")
 
-    if "!g+" in irc_msg:
+    if irc_msg == "!g+":
         irc.send(chan_msg+"https://plus.google.com/communities/116969234888661099943 \n")
 
-    if ":!curr" in irc_msg:
+    if irc_msg == ":!curr":
         irc.send(chan_msg+"https://moot.it/learnpython \n")
     
-    if ":!dog" in irc_msg:
+    if irc_msg == ":!dog":
         irc.send(chan_msg+"Woof woof woof WOOF! \n")
 
-    if ":!help" in irc_msg:
+    if irc_msg == ":!help":
         irc.send(chan_msg+"My commands are: !git, !g+, !curr, !lib, !dog and !help \n")
 
-    if (":!die "+nick) in irc_msg:
+    if irc_msg == (":!die "+nick):
         alive = False
 
-    if ":!wut" in irc_msg:
+    if irc_msg == ":!wut":
         irc.send(chan_msg+"tecn1c0 doesn't understand \n")
-    if "!lib"in irc_msg:
+    if irc_msg == "!lib":
         irc.send(chan_msg+"http://www.lfd.uci.edu/~gohlke/pythonlibs/ \n")
 
     if irc_msg.find("PING :") != -1:
